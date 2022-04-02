c2df04deb2872024f0f19194cb53c98710068615
Nikita Shumilin <firenesfall@gmail.com>
2022-04-02 13:12:03 +0300

    Remove some fpc compiled files. Add procedure examples.

---
+++ book1/part2/subprograms/diamond_procedure.pas
@@ -0,0 +1,43 @@
+program diamond_procedure.pas;
+
+procedure PrintChars(ch: char; count: integer);
+var
+	i: integer;
+begin
+	for i:= 1 to count do
+		write(ch)
+end;
+
+procedure PrintLineOfDiamond(i, n: integer);
+begin
+	PrintChars(' ', n + 1 - i);
+	write('*');
+
+	if i > 1 then
+	begin
+		PrintChars(' ', 2 * i - 3);
+		write('*');
+	end;
+	writeln;
+end;
+
+var
+	n, h, i: integer;
+
+begin
+	{ ask user to enter odd number }
+	repeat
+		writeln('Enter the diamond''s height (positive odd): ');
+		readln(h);
+		writeln('Enter an odd number');
+	until (h > 0) and (h mod 2 <> 0);
+
+	writeln('Your number is odd ', h);
+
+	n := h div 2;
+
+	for i := 1 to n + 1 do
+		PrintLineOfDiamond(i, n);
+	for i := n downto 1 do
+		PrintLineOfDiamond(i, n)
+end.
---
+++ book1/part2/subprograms/procedure_hello.pas
@@ -0,0 +1,10 @@
+program procedure_hello.pas;
+
+procedure SayHello;
+begin
+	writeln('Hello, world!')
+end;
+
+begin
+	SayHello
+end.
