13480ab9c8c59c2a88200d286bceadbd15690d34
Nikita Shumilin <firenesfall@gmail.com>
2022-12-16 19:59:06 +0300

    Add 2.5.1

---
+++ book1/part2/symbols_with_codes_and_text_data/print_ascii.pas
@@ -0,0 +1,31 @@
+program PrintAscii;	{ print_ascii.pas }
+
+var
+	i, j: integer;
+	c: char;
+
+begin
+	{ first line of header }
+	write('  |');
+	for c := '0' to '9' do
+		write(' .', c);
+	for c := 'A' to 'F' do
+		write(' .', c);
+	writeln;
+
+	{ second line of header }
+	write('  |');
+	for i := 1 to 16 do
+		write('---');
+	writeln;
+
+	{ table }
+	for i := 2 to 7 do
+	begin
+		write(i , '.|');
+		{ print line of table }
+		for j := 0 to 15 do
+			write('  ', char(i*16 + j));
+		writeln
+	end
+end.
