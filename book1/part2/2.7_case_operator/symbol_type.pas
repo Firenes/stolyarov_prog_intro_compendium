1b55cbbd284b95ada23a8e2bb229ce479d4b998d
Nikita Shumilin <firenesfall@gmail.com>
2023-02-12 17:49:04 +0300

    Done 2.7

---
+++ book1/part2/2.7_case_operator/symbol_type.pas
@@ -0,0 +1,36 @@
+program SymbolType; { symbol_type.pas }
+
+var
+	c: char;
+
+begin
+	read(c);
+	write('The symbol is ');
+	case c of
+		'a'..'z', 'A'..'Z':
+			writeln('a latin letter');
+		'0'..'9':
+			writeln('a digit');
+		'+', '-', '/', '*':
+			writeln('an arithmetic operation symbol');
+		'<', '>', '=':
+			writeln('a comparision sign');
+		'.', ',', ';', ':', '!', '?':
+			writeln('a punctuation symbol');
+		'_', '~', '@', '#', '$', '%', '^',
+		'&', '`', '|', '\':
+			writeln('a special purpouse sign');
+		' ':
+			writeln('the space character');
+		#9, #10, #13:
+			writeln('a formatting code');
+		#27:
+			writeln('the escape code');
+		'(', ')', '[', ']', '{', '}':
+			writeln('a grouping symbol');
+		'"', '''':
+			writeln('a quoting symbol');
+		else
+			writeln('something strange')
+	end
+end.
