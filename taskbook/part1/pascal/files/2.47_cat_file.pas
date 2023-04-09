fa6c5101cf7b0884174b05adc9db5a0db42ee049
Nikita Shumilin <firenesfall@gmail.com>
2023-04-09 21:18:35 +0300

    Done 2.47

---
+++ taskbook/part1/pascal/files/2.47_cat_file.pas
@@ -0,0 +1,32 @@
+program CatFile; { 2.47_cat_file.pas }
+
+{ Main }
+
+var
+	f: text;
+	content: string;
+begin
+	{$I-}
+	if ParamCount <> 1 then
+	begin
+		writeln(ErrOutput, 'Need file name');
+		halt(1)
+	end;
+
+	assign(f, ParamStr(1));
+	reset(f);
+
+	if IOResult <> 0 then
+	begin
+		writeln(ErrOutput, 'Couldn''t open a file', ParamStr(1));
+		halt(1)
+	end;
+
+	while not SeekEof(f) do
+	begin
+		readln(f, content);
+		writeln(content);
+	end;
+
+	close(f)
+end.
